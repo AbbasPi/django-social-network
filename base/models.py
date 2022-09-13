0f8623dbbe810d325b06584a84bdc7fb2bc75039
Abbas Salah <abbas.log01@gmail.com>
2022-09-13 21:23:10 +0300

    User Profile & Basic Layout

--- base/models.py
+++ base/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+
 class BaseModel(models.Model):
     class Meta:
         abstract = True
     created = models.DateTimeField(auto_now_add=True, editable=False)
     updated = models.DateTimeField(auto_now=True, editable=False)
-    
-        
+
+
 class Topic(BaseModel):
     name = models.CharField(max_length=200)
-    
+
     def __str__(self):
         return str(self.name)
 
@@ -19,19 +21,23 @@ class Room(BaseModel):
     topic = models.ForeignKey('Topic', on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    # participants =
-    
+    participants = models.ManyToManyField(
+        User, related_name='participants', blank=True)
+
     class Meta:
         ordering = ['-updated', '-created']
-    
+
     def __str__(self):
         return str(self.name)
-    
-    
+
+
 class Message(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     content = models.TextField()
-    
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
     def __str__(self):
-        return str(self.content)
+        return self.content[0:50]
--- base/urls.py
+++ base/urls.py
@@ -5,10 +5,12 @@ urlpatterns = [
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
     path('register/', views.registerPage, name='register'),
+    path('profile/<str:pk>/', views.userProfile, name='profile'),
 
     path('', views.home, name='home'),
     path('room/<int:pk>/', views.room, name='room'),
     path('create-room/', views.createRoom, name='create-room'),
     path('update-room/<str:pk>', views.updateRoom, name='update-room'),
     path('delete-room/<str:pk>', views.deleteRoom, name='delete-room'),
+    path('delete-message/<str:pk>', views.deleteMessage, name='delete-message'),
 ]
