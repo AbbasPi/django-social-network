7254eb81abce70a438ef17ec2898aa2fa508c90b
Abbas Salah <abbas.log01@gmail.com>
2022-09-20 21:14:53 +0300

    mobile responsive

--- base/views.py
+++ base/views.py
@@ -1,4 +1,3 @@
-from pydoc_data.topics import topics
 from django.shortcuts import render, redirect
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
@@ -62,7 +61,7 @@ def home(request):
     rooms = Room.objects.filter(Q(topic__name__icontains=q) |
                                 Q(name__icontains=q) |
                                 Q(description__icontains=q))
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
 
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
@@ -176,3 +175,14 @@ def updateUser(request):
             form.save()
             return redirect('profile', pk=user.id)
     return render(request, 'base/update_user.html', {'form': form})
+
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics': topics})
+
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    return render(request, 'base/activity.html', {'room_messages': room_messages})
